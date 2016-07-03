cdcd27362cca85704bdcf1f474508889118f4ef2
YoYash <yash.sharma99@yahoo.com>
2016-07-03 22:20:41 +0530

    Update script.js

--- Module-4/js/script.js
+++ Module-4/js/script.js
@@ -52,7 +52,7 @@ for (var i = 0; i < names.length; i++) {
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = names[i].charAt(0).toLowerCase();
-  //console.log(firstLetter +" " + names);
+  // console.log(firstLetter +" " + names);
 
   // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
@@ -60,10 +60,11 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
+    byeSpeaker.speak(names[i]); 
   } else {
     helloSpeaker.speak(names[i]);
   }
 }
+
 
 })();
